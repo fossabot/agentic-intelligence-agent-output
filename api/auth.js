99ba586f5988a7437481ab019bd5c4a0065099aa
StopAnchorPanda <59894539+grabngostuff@users.noreply.github.com>
2026-03-24 16:29:36 -0500

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -7,7 +7,7 @@ module.exports = async function handler(req, res) {
     const tokenRequest = await new Promise((resolve, reject) => {
       client.auth.createTokenRequest(
         {
-          capability: { 'agent-channel': ['subscribe'] }
+          capability: '{"agent-channel":["subscribe"]}'
         },
         (err, tokenRequest) => {
           if (err) reject(err);
@@ -16,12 +16,7 @@ module.exports = async function handler(req, res) {
       );
     });
 
-    // Make sure capability is an object not a string
-    if (typeof tokenRequest.capability === 'string') {
-      tokenRequest.capability = JSON.parse(tokenRequest.capability);
-    }
-
-    console.log('Token request created:', JSON.stringify(tokenRequest));
+    console.log('Token request created successfully');
     res.status(200).json(tokenRequest);
 
   } catch (error) {
